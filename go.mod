72fc7ddeeb3305bfd8e5be6cfd2b6fd7ab1537ac
Luis Correia Lima Neto <mr.jonze@gmail.com>
2024-05-13 20:48:06 -0300

    testando rate limiter como middleware

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/mrjonze/goexpert/rate-limiter
 go 1.22.2
 
 require (
+	github.com/go-chi/chi v1.5.5
 	github.com/go-redis/redis/v8 v8.11.5
 	github.com/spf13/viper v1.18.2
 )
--- server/main.go
+++ server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/mrjonze/goexpert/rate-limiter/server/config"
 	"net/http"
 )
@@ -15,14 +17,25 @@ func main() {
 
 	fmt.Println("Token name: ", configs.TokenName)
 
-	http.HandleFunc("/", readHeader)
-	http.ListenAndServe(":8080", nil)
+	r := chi.NewRouter()
+	r.Use(middleware.Recoverer)
+	r.Use(middleware.Logger)
+	r.Use(rateLimiter)
+
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("ok"))
+	})
+
+	http.ListenAndServe(":8080", r)
 }
 
-func readHeader(w http.ResponseWriter, r *http.Request) {
-	for name, headers := range r.Header {
-		for _, h := range headers {
-			fmt.Printf("%v: %v\n", name, h)
+func rateLimiter(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		for name, headers := range r.Header {
+			for _, h := range headers {
+				fmt.Printf("%v: %v\n", name, h)
+			}
 		}
-	}
+		next.ServeHTTP(w, r)
+	})
 }
